d6e24797fc6279930141044137b63901b53adfeb
Aravindan Chockalingam <aravindan.chockalingam@reddeer.com>
2024-03-04 11:24:31 +0000

    Amended .gitignore to include python specific exclusions along with main python file

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+import os
+import subprocess
+import json
+
+
+def get_git_diff() -> str:
+    diff_output = subprocess.run(['git', 'diff'], capture_output=True, text=True)
+    return diff_output.stdout.strip()
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+model = os.getenv('MODEL_NAME')
+url = os.getenv('ENDPOINT_URL')
+stream = os.getenv('STREAM')
+
+json_payload = {
+    "model": model,
+    "prompt": "Generate and show only a concise git commit message written in present tense without return characters for the git diff: {}".format(get_git_diff()),
+    "stream": False,
+}
+
+response = requests.post(url, json=json_payload)
+git_message = json.loads(response.text)['response']
+print(git_message)
